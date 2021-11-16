7a86ae37169dfab56089297eb22e75dd025d80b5
Munif Tanjim <hello@muniftanjim.dev>
2021-11-16 23:01:19 +0600

    style: use variable braces consistently

--- zed.zsh
+++ zed.zsh
@@ -15,13 +15,13 @@ function __zed_log_info() {
 
 # completion [[[
 function __zed_compinit() {
-  for zcompdump in $ZED[CACHE_DIR]/.zcompdump(N.mh+24); do
+  for zcompdump in ${ZED[CACHE_DIR]}/.zcompdump(N.mh+24); do
     compinit -d "${zcompdump}"
     if [[ ! -s "${zcompdump}.zwc" ]] || [[ "${zcompdump}" -nt "${zcompdump}.zwc" ]]; then
       zcompile "${zcompdump}"
     fi
   done
-  compinit -C -d "$ZED[CACHE_DIR]/.zcompdump"
+  compinit -C -d "${ZED[CACHE_DIR]}/.zcompdump"
 }
 
 function __zed_compdef() {
@@ -37,7 +37,7 @@ function __zed_compdef_intercept_on() {
 
 function __zed_compdef_intercept_off() {
   if (( ${+ZED[function-compdef]} )); then
-    functions[compdef]="$ZED[function-compdef]"
+    functions[compdef]="${ZED[function-compdef]}"
   else
     unfunction compdef
   fi
@@ -58,98 +58,98 @@ function __zed_compdef_replay() {
 
 # registry [[[
 function :zed_plugin_registry() {
-  local action="$1"; shift
+  local action="${1}"; shift
 
   local key val
 
-  case $action in
+  case ${action} in
     get)
-      key="$1"
-      ZED_CTX=("${(@Q)${(@z)_zed_plugin_registry[$key]}}")
+      key="${1}"
+      ZED_CTX=("${(@Q)${(@z)_zed_plugin_registry[${key}]}}")
       ;;
     set)
-      key="$ZED_CTX[id]"
+      key="${ZED_CTX[id]}"
       val="${(j: :)${(@qkv)ZED_CTX}}"
-      _zed_plugin_registry[$key]="$val"
+      _zed_plugin_registry[${key}]="${val}"
       ;;
   esac
 }
 # ]]]
 
 function :zed_install() {
-  local plugin_dir="$ZED[DATA_DIR]/plugins/$ZED_CTX[name]"
+  local plugin_dir="${ZED[DATA_DIR]}/plugins/${ZED_CTX[name]}"
 
   if [[ ! -d "${plugin_dir:h}" ]]; then
     mkdir -p "${plugin_dir:h}"
   fi
 
-  local name="$ZED_CTX[name]"
+  local name="${ZED_CTX[name]}"
 
-  __zed_log_info "$name installing..."
+  __zed_log_info "${name} installing..."
 
-  local src_uri="$ZED_CTX[src]"
+  local src_uri="${ZED_CTX[src]}"
 
-  if [[ "$ZED_CTX[from]" = "git" ]]; then
-    if [[ $src_uri != *://* ]]; then
+  if [[ "${ZED_CTX[from]}" = "git" ]]; then
+    if [[ ${src_uri} != *://* ]]; then
       src_uri="https://${src_uri%.git}.git"
     fi
 
-    command git -C "${plugin_dir:h}" clone --depth 1 --recursive --shallow-submodules "$src_uri" "${plugin_dir:t}"
+    command git -C "${plugin_dir:h}" clone --depth 1 --recursive --shallow-submodules "${src_uri}" "${plugin_dir:t}"
 
     if [[ $? -ne 0 ]]; then
-      __zed_log_err "$name install failed"
+      __zed_log_err "${name} install failed"
       return 1
     fi
   fi
 
-  if [[ "$ZED_CTX[from]" = "file" ]]; then
-    if [[ -f "$src_uri" ]]; then
-      mkdir -p "$plugin_dir"
-      cp "$src_uri" "$plugin_dir/$ZED_CTX[name].plugin.zsh"
-    elif [[ -d "$src_uri" ]]; then
-      mkdir -p "$plugin_dir"
-      cp -r $src_uri/** "$plugin_dir/"
+  if [[ "${ZED_CTX[from]}" = "file" ]]; then
+    if [[ -f "${src_uri}" ]]; then
+      mkdir -p "${plugin_dir}"
+      cp "${src_uri}" "${plugin_dir}/${ZED_CTX[name]}.plugin.zsh"
+    elif [[ -d "${src_uri}" ]]; then
+      mkdir -p "${plugin_dir}"
+      cp -r ${src_uri}/** "${plugin_dir}/"
     fi
   fi
 
-  __zed_log_info "$name installed"
+  __zed_log_info "${name} installed"
 }
 
 function :zed_update() {
-  if [[ $__zed_skip_update = true ]]; then
+  if [[ ${__zed_skip_update} = true ]]; then
     return 0
   fi
 
-  local plugin_dir="$ZED[DATA_DIR]/plugins/$ZED_CTX[name]"
+  local plugin_dir="${ZED[DATA_DIR]}/plugins/${ZED_CTX[name]}"
 
-  local name="$ZED_CTX[name]"
+  local name="${ZED_CTX[name]}"
 
-  __zed_log_info "$name updating..."
+  __zed_log_info "${name} updating..."
 
-  local src_uri="$ZED_CTX[src]"
+  local src_uri="${ZED_CTX[src]}"
 
-  if [[ "$ZED_CTX[from]" = "git" ]]; then
-    command git -C "$plugin_dir" pull --quiet --recurse-submodules --depth 1 --rebase --autostash
+  if [[ "${ZED_CTX[from]}" = "git" ]]; then
+    command git -C "${plugin_dir}" pull --quiet --recurse-submodules --depth 1 --rebase --autostash
 
     if [[ $? -ne 0 ]]; then
-      __zed_log_err "$name update failed"
+      __zed_log_err "${name} update failed"
       return 1
     fi
   fi
 
-  if [[ "$ZED_CTX[from]" = "file" ]]; then
-    if [[ -f "$src_uri" ]]; then
-      cp "$src_uri" "$plugin_dir/$ZED_CTX[name].plugin.zsh"
-    elif [[ -d "$src_uri" ]]; then
-      cp -r $src_uri/** "$plugin_dir/"
+  if [[ "${ZED_CTX[from]}" = "file" ]]; then
+    if [[ -f "${src_uri}" ]]; then
+      cp "${src_uri}" "${plugin_dir}/${ZED_CTX[name]}.plugin.zsh"
+    elif [[ -d "${src_uri}" ]]; then
+      cp -r ${src_uri}/** "${plugin_dir}/"
     fi
   fi
 
-  __zed_log_info "$name updated"
+  __zed_log_info "${name} updated"
 }
 
 function :zed_install_or_update() {
-  local plugin_dir="$ZED[DATA_DIR]/plugins/$ZED_CTX[name]"
+  local plugin_dir="${ZED[DATA_DIR]}/plugins/${ZED_CTX[name]}"
 
   if [[ "${ZED_CTX[src][1]}" = "/" ]] || [[ "${ZED_CTX[src][1]}" = "~" ]]; then
     ZED_CTX[from]="file"
@@ -157,7 +157,7 @@ function :zed_install_or_update() {
     ZED_CTX[from]="git"
   fi
 
-  if [[ -d "$plugin_dir" ]]; then
+  if [[ -d "${plugin_dir}" ]]; then
     :zed_update
   else
     :zed_install
@@ -167,16 +167,16 @@ function :zed_install_or_update() {
     return $?
   fi
 
-  pushd "$plugin_dir$ZED_CTX[dir]" > /dev/null
+  pushd "${plugin_dir}${ZED_CTX[dir]}" > /dev/null
 
-  eval "$ZED_CTX[onpull]"
+  eval "${ZED_CTX[onpull]}"
 
-  if [[ ! -f "$ZED_CTX[pick]" ]]; then
+  if [[ ! -f "${ZED_CTX[pick]}" ]]; then
     return 1
   fi
 
   for file in ${(s: :)${ZED_CTX[compile]:-${ZED_CTX[pick]}}}; do
-    zcompile -U "$file"
+    zcompile -U "${file}"
   done
 
   popd > /dev/null
@@ -200,71 +200,71 @@ function _zed_pull() {
 
   local id
   for id in ${ids[@]}; do
-    :zed_plugin_registry get "$id"
+    :zed_plugin_registry get "${id}"
 
-    __zed_skip_update=$pulled_ids[$ZED_CTX[name]]
+    __zed_skip_update=${pulled_ids[${ZED_CTX[name]}]}
 
     :zed_install_or_update
 
-    pulled_ids[$ZED_CTX[name]]=true
+    pulled_ids[${ZED_CTX[name]}]=true
   done
 }
 
 function _zed_load() {
-  ZED_CTX[src]="$1"; shift
+  ZED_CTX[src]="${1}"; shift
 
   local ctx_key ctx_val
   while (( $# )); do
     ctx_key="${1%%:*}"
     ctx_val="${1#*:}"
-    ZED_CTX[$ctx_key]="$ctx_val"
+    ZED_CTX[${ctx_key}]="${ctx_val}"
     shift
   done
 
-  if [[ -z "$ZED_CTX[name]" ]]; then
+  if [[ -z "${ZED_CTX[name]}" ]]; then
     ZED_CTX[name]="${${ZED_CTX[src]:t}%.git}"
   fi
 
-  if [[ -z "$ZED_CTX[pick]" ]]; then
-    if [[ -z "$ZED_CTX[dir]" ]]; then
-      ZED_CTX[pick]="$ZED_CTX[name].plugin.zsh"
+  if [[ -z "${ZED_CTX[pick]}" ]]; then
+    if [[ -z "${ZED_CTX[dir]}" ]]; then
+      ZED_CTX[pick]="${ZED_CTX[name]}.plugin.zsh"
     else
       ZED_CTX[pick]="${ZED_CTX[dir]:t}.plugin.zsh"
     fi
   fi
 
-  ZED_CTX[dir]="${ZED_CTX[dir]:+/$ZED_CTX[dir]}"
+  ZED_CTX[dir]="${ZED_CTX[dir]:+/${ZED_CTX[dir]}}"
 
   ZED_CTX[id]="${ZED_CTX[name]}${ZED_CTX[dir]:+:::${ZED_CTX[dir]:t}}"
 
   :zed_plugin_registry set
 
-  local plugin_dir="$ZED[DATA_DIR]/plugins/$ZED_CTX[name]"
+  local plugin_dir="${ZED[DATA_DIR]}/plugins/${ZED_CTX[name]}"
 
-  if [[ ! -f "$plugin_dir$ZED_CTX[dir]/$ZED_CTX[pick]" ]]; then
+  if [[ ! -f "${plugin_dir}${ZED_CTX[dir]}/${ZED_CTX[pick]}" ]]; then
     :zed_install_or_update
   fi
 
-  if [[ ! -f "$plugin_dir$ZED_CTX[dir]/$ZED_CTX[pick]" ]]; then
+  if [[ ! -f "${plugin_dir}${ZED_CTX[dir]}/${ZED_CTX[pick]}" ]]; then
     __zed_log_err "failed to load plugin:"
-    __zed_log_err "  $ZED_CTX[src] ${ZED_CTX[name]:+name:$ZED_CTX[name]}"
+    __zed_log_err "  ${ZED_CTX[src]} ${ZED_CTX[name]:+name:${ZED_CTX[name]}}"
     return 1
   fi
 
-  fpath+="$plugin_dir"
+  fpath+="${plugin_dir}"
 
-  if [[ -d "$plugin_dir/functions" ]]; then
-    fpath+="$plugin_dir/functions"
+  if [[ -d "${plugin_dir}/functions" ]]; then
+    fpath+="${plugin_dir}/functions"
   fi
 
   __zed_compdef_intercept_on
-  pushd "$plugin_dir$ZED_CTX[dir]" > /dev/null
-  source "$ZED_CTX[pick]"
-  eval "$ZED_CTX[onload]"
+  pushd "${plugin_dir}${ZED_CTX[dir]}" > /dev/null
+  source "${ZED_CTX[pick]}"
+  eval "${ZED_CTX[onload]}"
   popd > /dev/null
   __zed_compdef_intercept_off
 
-  ZED[plugin-$ZED_CTX[id]]=true
+  ZED[plugin-${ZED_CTX[id]}]=true
 }
 
 function _zed_done() {
@@ -276,33 +276,33 @@ function _zed_init() {
   declare -gA _zed_plugin_registry
   declare -ga ZED_COMPDEF_REPLAY
 
-  if [[ -z "$ZED[CACHE_DIR]" ]]; then
-    ZED[CACHE_DIR]="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/.zed"
+  if [[ -z "${ZED[CACHE_DIR]}" ]]; then
+    ZED[CACHE_DIR]="${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/.zed"
   fi
 
-  if [[ -z "$ZED[DATA_DIR]" ]]; then
-    ZED[DATA_DIR]="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/.zed"
+  if [[ -z "${ZED[DATA_DIR]}" ]]; then
+    ZED[DATA_DIR]="${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/.zed"
   fi
 
-  if [[ ! -d "$ZED[CACHE_DIR]" ]]; then
-    mkdir -p "$ZED[CACHE_DIR]"
+  if [[ ! -d "${ZED[CACHE_DIR]}" ]]; then
+    mkdir -p "${ZED[CACHE_DIR]}"
   fi
 
   autoload -Uz compinit
 }
 
-function $ZED[name]() {
-  local cmd="$1"
+function ${ZED[name]}() {
+  local cmd="${1}"
   local REPLY
 
-  if (( $+functions[_zed_$cmd] )); then
+  if (( ${+functions[_zed_${cmd}]} )); then
     shift
     local -A ZED_CTX
     ZED_CTX=()
-    _zed_$cmd $@
+    _zed_${cmd} $@
     return $?
   else
-    __zed_log_err "zed unknown command: $cmd"
+    __zed_log_err "zed unknown command: ${cmd}"
     return 1
   fi
 }
